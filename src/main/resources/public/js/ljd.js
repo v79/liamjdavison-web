4b3caa5b9ad1c28be9fd8391db523453dabc0429
v79 <liamjdavison@gmail.com>
2018-07-28 15:25:14 +0100

    Some sort of banner in place now

---
+++ src/main/resources/public/js/ljd.js
@@ -0,0 +1,26 @@
+/* ========================================== 
+scrollTop() >= 300
+Should be equal the the height of the header
+========================================== */
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        // we have scrolled down the page
+        $('nav').addClass('fixed-header');
+        $('nav').removeClass('translucent');
+        $('nav').addClass('opaque');
+
+         $('#site-title').removeClass('invisible');
+
+    }
+    else {
+    // we are at the top of the page
+        $('nav').removeClass('fixed-header');
+         $('nav').addClass('translucent');
+        $('nav').removeClass('opaque');
+
+
+      $('#site-title').addClass('invisible');
+
+    }
+});
